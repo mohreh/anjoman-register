b100eacc9119cf85f384d1b0be8e1e4eb66b9302
Mohammad Reza Hassani <reza_hassani_@outlook.com>
2022-04-13 21:20:36 +0430

    fix

--- server/app.module.ts
+++ server/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RegisterModule } from './register/register.module';
 import { AuthService } from './auth/auth.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [RegisterModule, AuthModule],
+  imports: [AuthModule],
   controllers: [AppController, AuthController],
   providers: [AppService, AuthService],
 })
