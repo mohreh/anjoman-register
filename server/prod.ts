67ed3d4f423cf0b683516be198565292c4748fc7
Mohammad Reza Hassani <reza_hassani_@outlook.com>
2022-04-15 19:48:17 +0430

    trying to solve cors error

--- server/prod.ts
+++ server/prod.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ allowedHeaders: '*', credentials: true });
+  app.enableCors({
+    allowedHeaders: '*',
+    credentials: true,
+    origin: '*',
+    methods: '*',
+  });
   app.use(helmet());
   await app.init();
   return app.getHttpAdapter().getInstance();
